5796f23781d514311f44c107f8ed5854a46e58c3
AlexSedNZ <alex.sedighi@centrality.ai>
2020-10-18 15:05:01 +1300

    Update the runtime weights for vanity_registry following running the benchmarks

--- runtime/src/weights/vanity_registry.rs
+++ runtime/src/weights/vanity_registry.rs
@@ -13,22 +13,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0-rc5
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
 
-use frame_support::weights::Weight;
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+
+use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 pub struct WeightInfo;
 impl vanity_registry::WeightInfo for WeightInfo {
     fn commit() -> Weight {
-        0
+        (97_000_000 as Weight)
+            .saturating_add(DbWeight::get().reads(3 as Weight))
+            .saturating_add(DbWeight::get().writes(2 as Weight))
     }
-    fn reveal(_name_length: usize) -> Weight {
-        0
+    fn reveal(name_length: usize) -> Weight {
+        (91_000_000 as Weight + 100000 * name_length as Weight)
+            .saturating_add(DbWeight::get().reads(3 as Weight))
+            .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn renew() -> Weight {
-        0
+        (46_000_000 as Weight)
+            .saturating_add(DbWeight::get().reads(2 as Weight))
+            .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn unregister() -> Weight {
-        0
+        (145_000_000 as Weight)
+            .saturating_add(DbWeight::get().reads(4 as Weight))
+            .saturating_add(DbWeight::get().writes(3 as Weight))
     }
 }
